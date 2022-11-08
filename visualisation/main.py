692d2afa752ab23f39e0ee7223a71cafcd352f45
Henning <henninglulei@gmail.com>
2022-11-08 20:40:53 +0100

    changed file structure

--- visualisation/main.py
+++ visualisation/main.py
@@ -3,7 +3,7 @@ import sys
 import streamlit as st
 import pydeck as pdk
 from route import get_dataframe
-from style import map_styles, color1, color2
+from style import map_styles, get_color
 from datetime import datetime, timedelta
 
 sys.path.append('C:/Users/henlu/PycharmProjects/nextbikes/data')
@@ -31,49 +31,23 @@ columns = ['bike_id', 'start_time', 'start_lat', 'start_lon', 'start_name', 'sta
 trips = pd.DataFrame(trips, columns=columns)
 
 
-last_trip: pd.Series = trips.iloc[-1]
-last_trip.swa
-trips = pd.concat([trips, pd.DataFrame(last_trip, columns=columns)], ignore_index=True)
+last_trip = trips.iloc[-1]
+last_trip = [v for i, v in last_trip.items()]
+last_trip[2], last_trip[3] = last_trip[7], last_trip[8]
+trips.loc[len(trips)] = last_trip
 trips['id'] = list(range(len(trips)))
 
 max_id = len(routes) - 1
-
-
-GET_COLOR_JS = [
-    f"{color1[0]} + {color2[0] - color2[0]} * (id / {max_id})",
-    f"{color1[1]} + {color2[1] - color1[1]} * (id / {max_id})",
-    f"{color1[2]} + {color2[2] - color1[2]} * (id / {max_id})"
-    f"155 + 100 * (id / {max_id})"
-]
 
 st.sidebar.title('Layers:')
 
 line_vis = 1 if st.sidebar.checkbox('Lines', value=True) else 0
 
-comment = """
-min_date = datetime.strptime(routes['start_time'].min(), '%d.%m.%y %H:%M')
-max_date = datetime.strptime(routes['start_time'].max(), '%d.%m.%y %H:%M')
-if max_date != min_date:
-    timeslot = st.sidebar.slider('See trips from', min_value=min_date, max_value=max_date, value=(min_date, max_date), format='DD.MM.YY HH:MM', step=timedelta(hours=3))
-else:
-    timeslot = (min_date, max_date)
-
-
-filter_mask = (timeslot[1].strftime('%d.%m.%y %H:%M') >= routes['start_time']) & (routes['start_time'] >= timeslot[0].strftime('%d.%m.%y %H:%M'))
-routes = routes[filter_mask]
-"""
-
-map_style = st.sidebar.selectbox('Select Map Style:', ('Streets',
-                                                       'Light',
-                                                       'Dark',
-                                                       'Satellite',
-                                                       'Outdoors',
-                                                       'Satellite Streets',
-                                                       'Navigation Day',
-                                                       'Navigation Night'))
+map_style = st.sidebar.selectbox('Select Map Style:', list(map_styles.keys()), index=3)
 
 st.sidebar.title('Line Graph:')
 interval = st.sidebar.slider('Interval', 5, 60, 15, 5, '%i minutes') * 60
+
 
 line_layer = pdk.Layer(
     'LineLayer',
@@ -81,10 +55,10 @@ line_layer = pdk.Layer(
     get_source_position='[start_lon, start_lat]',
     get_target_position='[end_lon, end_lat]',
     get_width=10,
-    get_color=GET_COLOR_JS,
+    get_color=get_color(max_id),
     pickable=True,
     picking_radius=250,
-    opacity=line_vis, )
+    opacity=line_vis)
 
 scatterplot_layer = pdk.Layer(
     'ScatterplotLayer',
@@ -95,16 +69,7 @@ scatterplot_layer = pdk.Layer(
     get_fill_color=[22, 22, 22],
     pickable=False)
 
-text_layer = pdk.Layer(
-    'TextLayer',
-    data=trips,
-    get_position='[start_lon, start_lat]',
-    get_text='id',
-    get_size=300,
-    getTextAnchor='"middle"',
-    get_alignment_baseline='"bottom"')
-
-map_layers = [text_layer, scatterplot_layer, line_layer]
+map_layers = [scatterplot_layer, line_layer]
 
 tooltip = {
     "html": f"&#128690 {{bike_id}} <br> &#128197 {{start_time}} <br> &#128337 {{duration}} Minuten <br>  &#x2194  {{distance}} km",
